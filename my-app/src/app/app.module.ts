1e067720c14cce13ad9208cb2d3e5fcef713c81d
Ruslan Slashchov <slashchev.ruslan@gmail.com>
2020-12-08 15:20:51 +0200

    HW1 corrections

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { LogoComponent } from './shared/logo/logo.component';
-import { CoursePageComponent } from './shared/course-page/course-page.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SearchComponent } from './shared/search/search.component';
-import { CourseItemComponent } from './shared/course-item/course-item.component';
-import { LoadMoreComponent } from './shared/load-more/load-more.component';
-import { UserAreaComponent } from './shared/user-area/user-area.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { LogoComponent } from './shared/components/logo/logo.component';
+import { CoursePageComponent } from './courses-page/course-page/course-page.component';
+import { BreadcrumbsComponent } from './shared/components/breadcrumbs/breadcrumbs.component';
+import { SearchComponent } from './courses-page/search/search.component';
+import { CourseItemComponent } from './courses-page/course-item/course-item.component';
+import { LoadMoreComponent } from './shared/components/load-more/load-more.component';
+import { UserAreaComponent } from './shared/components/user-area/user-area.component';
 
 @NgModule({
   declarations: [
